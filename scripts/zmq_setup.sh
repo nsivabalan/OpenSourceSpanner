7c006d5abc020f0ccea5d1ad64ff7f33a3bbef00
Ubuntu <ubuntu@ip-172-31-22-175.us-west-2.compute.internal>
2014-02-25 09:09:17 +0000

    zmq setup

---
+++ scripts/zmq_setup.sh
@@ -0,0 +1,18 @@
+mkdir temp
+cd temp
+cp ../lib/zeromq-3.2.3.tar.gz
+tar xvzf zeromq-3.2.3.tar.gz
+cd zeromq-3.2.3
+sudo apt-get install libtool autoconf automake
+sudo aptitude install build-essential
+./configure
+make
+sudo make install
+sudo ldconfig
+cd ../
+git clone https://github.com/zeromq/jzmq.git
+cd jzmq
+./autogen.sh
+./configure
+make
+sudo make install
