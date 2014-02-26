123c779ab2adbc5bb723666219d07af7cc38ff77
sivabalan <n.siva.b@gmail.com>
2014-02-26 08:37:32 -0800

    set up fix

--- scripts/set_up_hbase.sh
+++ scripts/set_up_hbase.sh
@@ -1,8 +1,8 @@
 mkdir ~/hbase_setup
 cd ~/hbase_setup
-wget http://mirror.metrocast.net/apache/hbase/stable/hbase-0.94.16.tar.gz
-tar xfvz hbase-0.94.16.tar.gz
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/" >> hbase-0.94.16/conf/hbase-env.sh
-./hbase-0.94.16/bin/start-hbase.sh
-echo "create 'userTable', 'cf'" | ./hbase-0.94.16/bin/hbase shell
+wget http://mirror.metrocast.net/apache/hbase/stable/hbase-0.94.17.tar.gz
+tar xfvz hbase-0.94.17.tar.gz
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/" >> hbase-0.94.17/conf/hbase-env.sh
+./hbase-0.94.17/bin/start-hbase.sh
+echo "create 'userTable', 'cf'" | ./hbase-0.94.17/bin/hbase shell
 
