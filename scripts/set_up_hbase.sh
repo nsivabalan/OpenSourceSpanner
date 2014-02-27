f824a5523bbedb0314767695a7dcf268a88bede2
Ubuntu <ubuntu@ip-10-191-175-132.us-west-1.compute.internal>
2014-02-27 18:28:36 +0000

    setup hbase fix

--- scripts/set_up_hbase.sh
+++ scripts/set_up_hbase.sh
@@ -1,6 +1,6 @@
 mkdir ~/hbase_setup
 cd ~/hbase_setup
-wget http://mirror.metrocast.net/apache/hbase/stable/hbase-0.94.17.tar.gz
+cp ~/OpenSourceSpanner/lib/hbase-0.94.17.tar.gz .
 tar xfvz hbase-0.94.17.tar.gz
 echo "export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/" >> hbase-0.94.17/conf/hbase-env.sh
 ./hbase-0.94.17/bin/start-hbase.sh
