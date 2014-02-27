4700d3dbd542b82420677b354a8fef7d68e6c848
Ubuntu <ubuntu@ip-10-191-175-132.us-west-1.compute.internal>
2014-02-27 18:29:47 +0000

    setup scripts fix

--- scripts/osspanner_setup.sh
+++ scripts/osspanner_setup.sh
@@ -1,1 +1,9 @@
+yes | sudo apt-get install git
 git clone https://github.com/nsivabalan/OpenSourceSpanner.git
+git checkout -b mavenfix
+git pull origin mavenfix
+cd OpenSourceSpanner
+Y | sh scripts/java_setup.sh
+Y | sh scripts/set_up_hbase.sh
+sh scripts/zmq_set_up.sh
+sh scripts/jzmq_set_up.sh
