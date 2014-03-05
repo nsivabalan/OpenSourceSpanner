f59a4f71efb9922e829e981a7f8fece9afb289d3
sivabalan <n.siva.b@gmail.com>
2014-03-05 02:09:17 -0800

    zmq set up fix

--- scripts/zmq_setup.sh
+++ scripts/zmq_setup.sh
@@ -3,8 +3,8 @@ cd ~/temp
 cp ~/OpenSourceSpanner/lib/zeromq-3.2.3.tar.gz .
 tar xvzf zeromq-3.2.3.tar.gz
 cd zeromq-3.2.3
-Y | sudo apt-get install libtool autoconf automake
-Y | sudo aptitude install build-essential
+sudo apt-get install libtool autoconf automake
+sudo aptitude install build-essential
 ./configure
 make
 sudo make install
